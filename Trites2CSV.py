788c6736ac34e8545a2758e9bae2a3fd66563762
Logiota <johannes.ts@me.com>
2019-04-06 00:33:06 +0200

    first commit

---
+++ Trites2CSV.py
@@ -0,0 +1,33 @@
+
+
+
+tern=['A','B','C','D','E','F','G','H','I','J','K',        'L',         'M',  'N',        'O','P', 'Q',    'R',  'S',   'T', 'U',       'V',         'W',    'X',                           'Y', 'Z', '9']
+data=['1','2','3','4','5','6','7','8','9','.','Timestamp','Longi/Lati','PPM','Turbidity','0','PH','Error','\n','Speed','C','uSiemens','Volatility','Watts','Use ternary alphabet until 99','?', '?', ',']
+
+
+
+inputs = input("Paste ternary or data for conversion(checks for','): ")
+"""
+if ',' in (inputs)
+	print 'input looks like data\n'
+	outputs = 'Ternary: ' 
+
+	for character in inputs:
+		outputs += (tern[data.index(character)])
+
+	print(outputs)
+
+
+else:""" # autodetection of data input for translation to ternary
+print ('input looks like ternary\n')
+
+outputs = 'Data: ' 
+
+for character in inputs:					
+	if character == 'X'	:						#make an exception for charaters flollowing
+		while (character != '9'):
+				outputs += character
+	else
+		outputs += (data[tern.index(character)])#translate ternary string to data
+
+print(outputs)

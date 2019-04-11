47665a887348a1b66e8990192a4a0d952cf9f038
logiota <45556023+logiota@users.noreply.github.com>
2019-04-11 14:16:19 +0200

    S now represents "  "

--- Trytes2CSV.py
+++ Trytes2CSV.py
@@ -1,5 +1,5 @@
-tern=['A','B','C','D','E','F','G','H','I','J','K',        'L',         'M',  'N',        'O','P', 'Q',    'R',  'S',   'T', 'U',       'V',         'W',    'X',                           'Y', 'Z', '9']
-data=['1','2','3','4','5','6','7','8','9','.','Timestamp','Longi/Lati','PPM','Turbidity','0','PH','Error','\n','Speed','C','uSiemens','Volatility','Watts','Use ternary alphabet until 99','?', '?', ',']
+tern=['A','B','C','D','E','F','G','H','I','J','K',        'L',         'M',  'N',        'O','P', 'Q',    'R', 'S','T', 'U',       'V',         'W',    'X',                           'Y', 'Z', '9']
+data=['1','2','3','4','5','6','7','8','9','.','Timestamp','Longi/Lati','PPM','Turbidity','0','PH','Error','\n',' ','C','uSiemens','Volatility','Watts','Use ternary alphabet until 9','?', '?', ',']
 
 
 

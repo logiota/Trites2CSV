e38dcc89bfb208232b7700c9afe6d3acb37c0c3e
logiota <45556023+logiota@users.noreply.github.com>
2019-05-21 20:48:20 +0200

    Total rewrite of TextMode logic(now working) and starting automatic CSV to Trytes

--- Trytes2CSV.py
+++ Trytes2CSV.py
@@ -2,36 +2,40 @@ tern=['A','B','C','D','E','F','G','H','I','J','K',        'L',         'M',  'N'
 data=['1','2','3','4','5','6','7','8','9','.','Timestamp','Longi/Lati','PPM','Turbidity','0','PH','Error','\n',' ','C','uSiemens','Volatility','Watts','Use ternary alphabet until 9','?', '?', ',']
 
 
-
 inputs = input("Paste ternary or data for conversion(checks for','): ") #ask for input and get input
 """
-if ',' in (inputs) #look for ternary 
-	print 'input looks like data\n'
-	outputs = 'Ternary: ' 
-
-	for character in inputs:
-		outputs += (tern[data.index(character)])
-
-	print(outputs)
-
-
+if ',' in (inputs) #look for ternary data
+	print 'input looks like ASCII data\n'
+	#
+	
 else: # autodetection of data input for translation to ternary
-#This is an example of data turnary:
+"""
+
+#This is an example of data ternary:
 #XTEXT9SXAS9SXTEXT9SABCDEFJABC
 #Which should result in the following ASCII data:
 #TEXT AS TEXT 123456,123
-"""
 
-outputs = 'Data: ' 
 
-for character in inputs:							#make an exception for charaters following X
-	if character == 'X'	:
-		for character in inputs:			
-			while (character != '9'):			
-				outputs += character			#output the original A-Z trytes
-													#need of breaking while loop to avoid getting stuck here
-			break
-	else :
-	outputs += (data[tern.index(character)])	#translate ternary string to data
+
+TextMode = False					#starts in data mode
+
+outputs = 'Translated data: ' 
+
+for character in inputs:
+
+	if character == "9":
+		TextMode = False
+
+	if character == "X" and TextMode == False:
+		TextMode = True
+		print('X Found and removed')
+									#debug
+	elif TextMode == True:
+		outputs += (character)
+		
+	else:											#"9" appears or at the end of the Alphabet data
+		outputs += (data[tern.index(character)])	#translate ternary string to data
+	
 
 print(outputs)

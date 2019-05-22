bac691b26d1f07133579b1abd5b50e2b53ddad5e
logiota <45556023+logiota@users.noreply.github.com>
2019-05-22 13:32:56 +0200

    Bug fixes and new example

--- Trytes2CSV.py
+++ Trytes2CSV.py
@@ -2,19 +2,26 @@ tern=['A','B','C','D','E','F','G','H','I','J','K',        'L',         'M',  'N'
 data=['1','2','3','4','5','6','7','8','9','.','Timestamp','Longi/Lati','PPM','Turbidity','0','PH','Error','\n',' ','C','uSiemens','Volatility','Watts','Use ternary alphabet until 9','?', '?', ',']
 
 
+
 inputs = input("Paste ternary or data for conversion(checks for','): ") #ask for input and get input
 """
 if ',' in (inputs) #look for ternary data
-	print 'input looks like ASCII data\n'
-	#
-	
+	print 'input looks like data\n'
+	outputs = 'Ternary: ' 
+
+	for character in inputs:
+		outputs += (tern[data.index(character)])
+
+	print(outputs)
+
 else: # autodetection of data input for translation to ternary
 """
 
-#This is an example of data ternary:
-#XTEXT9SXAS9SXTEXT9SABCDEFJABC
-#Which should result in the following ASCII data:
-#TEXT AS TEXT 123456,123
+# This is an example of data turnary: 
+# XFL99XCLAY9SXCOUNTY9BFEHBAJEG9O9AEGEO
+# 
+# Which should result in the following ASCII data: 
+# FL,CLAY COUNTY,265821.57,0,15750
 
 
 
@@ -24,13 +31,15 @@ outputs = 'Translated data: '
 
 for character in inputs:
 
-	if character == "9":
-		TextMode = False
+
 
 	if character == "X" and TextMode == False:
 		TextMode = True
 		print('X Found and removed')
 									#debug
+	elif character == "9" and TextMode == True:
+		TextMode = False
+		
 	elif TextMode == True:
 		outputs += (character)
 		
